cb4d77f6d163e52fbb4c53378e8a62080df39be1
Tomáš Mikula <mail@tomasmikula.cz>
2019-11-25 16:31:03 +0100

    Fix cache problem

--- fcapy/similarity.py
+++ fcapy/similarity.py
@@ -1,15 +1,15 @@
 def similarity_smc(attrs1, attrs2, attributes_to_remove=None, cache=None):
+    if attributes_to_remove is None:
+        attributes_to_remove = attrs1.infimum
+
     if cache is not None:
-        attrs_id = 's' + str((attrs1 + attrs2) *
-                             (attrs1 + attrs2 + 1) / 2 + attrs1)
+        attrs_id = 'similarity_smc' + str((attrs1 + attrs2) *
+                                          (attrs1 + attrs2 + 1) / 2 + attrs1)
 
         cached_similarity = cache.get(attrs_id)
 
         if cached_similarity is not None:
             return cached_similarity
-
-    if attributes_to_remove is None:
-        attributes_to_remove = attrs1.infimum
 
     intersection = attrs1.fromint(
         attrs1 & attrs2).difference(attributes_to_remove)
@@ -32,9 +32,18 @@ def similarity_smc(attrs1, attrs2, attributes_to_remove=None, cache=None):
     return result
 
 
-def similarity_jaccard(attrs1, attrs2, attributes_to_remove=None):
+def similarity_jaccard(attrs1, attrs2, attributes_to_remove=None, cache=None):
     if attributes_to_remove is None:
         attributes_to_remove = attrs1.infimum
+
+    if cache is not None:
+        attrs_id = 'similarity_jaccard' + str((attrs1 + attrs2) *
+                                              (attrs1 + attrs2 + 1) / 2 + attrs1)
+
+        cached_similarity = cache.get(attrs_id)
+
+        if cached_similarity is not None:
+            return cached_similarity
 
     intersection = attrs1.fromint(
         attrs1 & attrs2).difference(attributes_to_remove)
