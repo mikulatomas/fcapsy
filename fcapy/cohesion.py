741d33c0e33aba4c4ee8fa6e4b8616e3e5807e21
Tomáš Mikula <mail@tomasmikula.cz>
2019-11-26 10:01:26 +0100

    Disable caching in similarity calculation

--- fcapy/cohesion.py
+++ fcapy/cohesion.py
@@ -26,7 +26,7 @@ def cohesion_min(concept, context, similarity_function, cache=None):
     combs = combinations_with_replacement(concept_objects, 2)
 
     # result = min(starmap(similarity_function, combs))
-    result = min([similarity_function(x, y, cache=cache) for x, y in combs])
+    result = min([similarity_function(x, y) for x, y in combs])
 
     if cache is not None:
         cache[concept_id] = result
@@ -46,6 +46,6 @@ def cohesion_avg(concept, context, similarity_function, cache=None):
     combs = combinations(concept_objects, 2)
 
     # suma = sum(starmap(similarity_function, combs))
-    suma = sum([similarity_function(x, y, cache=cache) for x, y in combs])
+    suma = sum([similarity_function(x, y) for x, y in combs])
 
     return suma / (len(concept.extent) * (len(concept.extent) - 1) / 2)
