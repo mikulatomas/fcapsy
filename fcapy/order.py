c0fcd8cf49c803d34efa9adefcbb0631aa50578e
Tomáš Mikula <mail@tomasmikula.cz>
2020-09-25 12:40:50 +0200

    Comparable neighbors

--- fcapy/order.py
+++ fcapy/order.py
@@ -4,6 +4,7 @@ from collections import deque, namedtuple
 from collections.abc import Mapping
 from itertools import combinations
 from functools import reduce
+from typing import Iterator
 
 LatticeNode = namedtuple("LatticeNode", ["upper", "lower"])
 
@@ -44,6 +45,35 @@ class Lattice(Mapping):
 
                 queue.append(neighbor)
 
+    def __comparable_upper_concepts(self, concept: Concept) -> Iterator[Concept]:
+        for neighbor in self[concept].upper:
+            for x in self.comparable_upper_concepts(neighbor):
+                yield x
+        yield concept
+
+    def comparable_upper_concepts(self, concept: Concept) -> set:
+        return set(self.__comparable_upper_concepts(concept))
+
+    def __comparable_lower_concepts(self, concept: Concept) -> Iterator[Concept]:
+        for neighbor in self[concept].lower:
+            for x in self.comparable_lower_concepts(neighbor):
+                yield x
+        yield concept
+
+    def comparable_lower_concepts(self, concept: Concept) -> set:
+        return set(self.__comparable_lower_concepts(concept))
+
+    def __comparable_concepts(self, concept: Concept) -> Iterator[Concept]:
+        if not concept is None:
+            for neighbor in self[concept].upper:
+                yield from self.comparable_upper_concepts(neighbor)
+
+            for neighbor in self[concept].lower:
+                yield from self.comparable_lower_concepts(neighbor)
+
+    def comparable_concepts(self, concept: Concept) -> set:
+        return set(self.__comparable_concepts(concept))
+
     def __getitem__(self, concept: Concept):
         return self._mapping[concept]
 
@@ -58,7 +88,7 @@ class Lattice(Mapping):
         return tuple(self.keys())
 
 
-class SubsetLattice(Mapping):
+class SubsetLattice(Lattice):
     """
     Experimental implementation of
     Rice, Michael D., and Michael Siff. "Clusters, concepts, and pseudometrics."
@@ -127,16 +157,3 @@ class SubsetLattice(Mapping):
                     new_concept)
 
             worklist.add(new_concept)
-
-    def __getitem__(self, concept: Concept):
-        return self._mapping[concept]
-
-    def __iter__(self):
-        return iter(self._mapping)
-
-    def __len__(self):
-        return len(self._mapping)
-
-    @property
-    def concepts(self) -> tuple:
-        return tuple(self.keys())
