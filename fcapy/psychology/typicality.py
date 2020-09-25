a5579f398672e66b7e5a0fb4bdf55af270bf1496
Tomáš Mikula <mail@tomasmikula.cz>
2020-09-25 12:40:26 +0200

    Typicality added

---
+++ fcapy/psychology/typicality.py
@@ -0,0 +1,57 @@
+# Typicality implementation
+#
+# Rosch, Eleanor, and Carolyn B. Mervis. "Family resemblances: Studies in the internal structure of categories."
+# Cognitive psychology 7.4 (1975): 573-605.
+#
+# Belohlavek, Radim, and Tomas Mikula. "Typicality in conceptual structures within the framework of formal concept analysis."
+
+from itertools import compress
+import math
+
+
+def _calculate_similarities(item, items_to_compare, similarity_function):
+    return map(lambda other: similarity_function(item, other), items_to_compare)
+
+
+def typicality_avg(item, items_to_compare, similarity_function):
+    if type(item) is not type(items_to_compare[0]):
+        raise ValueError("Wrong type of items!")
+
+    similarities = _calculate_similarities(
+        item, items_to_compare, similarity_function)
+
+    return sum(similarities) / len(items_to_compare)
+
+
+def typicality_min(item, items_to_compare, similarity_function):
+    if type(item) is not type(items_to_compare[0]):
+        raise ValueError("Wrong type of items!")
+
+    similarities = _calculate_similarities(
+        item, items_to_compare, similarity_function)
+
+    return min(similarities)
+
+
+def _calculate_weights(objects):
+    objects = map(lambda x: x.bools(), objects)
+    return [sum(y) for y in zip(*objects)]
+
+
+def typicality_rosch(item, items_to_compare):
+    if type(item) is not type(items_to_compare[0]):
+        raise ValueError("Wrong type of items!")
+
+    weights = _calculate_weights(items_to_compare)
+
+    return sum(compress(weights, item.bools()))
+
+
+def typicality_rosch_ln(item, items_to_compare):
+    if type(item) is not type(items_to_compare[0]):
+        raise ValueError("Wrong type of items!")
+
+    weights = _calculate_weights(items_to_compare)
+    weights = map(lambda x: math.log(x) if x != 0 else -math.inf, weights)
+
+    return sum(compress(weights, item.bools()))
