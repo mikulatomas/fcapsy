47864389ba04b760870812b3623af3314b1ca3ec
Tomáš Mikula <mail@tomasmikula.cz>
2020-11-07 23:03:38 +0100

    Add random test for algs

---
+++ tests/algorithms/test_random_alg_test.py
@@ -0,0 +1,18 @@
+import pytest
+from fcapy import Context, Lattice
+from fcapy.algorithms.fcbo import fcbo
+from fcapy.algorithms.rice_siff import concept_subset
+from fcapy.similarity.objects import jaccard
+
+
+random_data = [Context.from_random(20, 10) for i in range(10)]
+
+
+@pytest.mark.parametrize("context", random_data)
+def test_random_alg_test(context):
+    fcbo_result = set(fcbo(context))
+    lattice_result = set(Lattice(context).concepts)
+    subset_result = set(concept_subset(context, jaccard))
+
+    assert fcbo_result == lattice_result
+    assert subset_result.issubset(fcbo_result)
