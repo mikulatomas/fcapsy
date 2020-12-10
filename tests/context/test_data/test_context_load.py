7a518498b20c9eb67a074f9be88c20a59d87ac87
Tomáš Mikula <mail@tomasmikula.cz>
2020-12-10 13:38:43 +0100

    Add tests for Context.from_pandas

--- tests/context/test_data/test_context_load.py
+++ tests/context/test_data/test_context_load.py
@@ -4,6 +4,7 @@ from tests import load_all_test_files
 import os
 import pytest
 import json
+import pandas as pd
 
 TEST_DATA_DIR_FIMI = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -81,3 +82,37 @@ def test_context_from_csv_delimiter(data_file, json_file):
     assert list(context._Objects.supremum.members(
     )) == expected_json['objects']
     assert context.get_bools() == tuple(map(tuple, expected_json['bools']))
+
+
+def test_context_from_pandas():
+    bools = (
+        (2, 0, 6),
+        (132, 1, 0)
+    )
+
+    expected_bools = (
+        (1, 0, 1),
+        (1, 1, 0)
+    )
+
+    context = Context.from_pandas(pd.DataFrame(bools))
+
+    assert context.get_bools() == expected_bools
+
+
+def test_context_from_pandas_truth_values():
+    bools = (
+        (2, 0, 6),
+        (132, 1, 0)
+    )
+
+    expected_bools = (
+        (0, 0, 1),
+        (1, 0, 0)
+    )
+
+    df = pd.DataFrame(bools)
+
+    context = Context.from_pandas(df > 2)
+
+    assert context.get_bools() == expected_bools
