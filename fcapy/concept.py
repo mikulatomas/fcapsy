acfc063aaaeeec7fd24cf80568154239774e71b2
Tomáš Mikula <mail@tomasmikula.cz>
2021-02-12 16:24:15 +0100

    Add name property to Concept

--- fcapy/concept.py
+++ fcapy/concept.py
@@ -5,39 +5,38 @@ from fcapy.context import Context
 
 
 class Concept:
-    def __init__(self, extent: Type[BitSet], intent: Type[BitSet]):
-        self._extent = extent
-        self._intent = intent
+    def __init__(self, extent: Type[BitSet], intent: Type[BitSet], name: str = None):
+        self.extent = extent
+        self.intent = intent
+        self.name = name
 
     @classmethod
-    def from_intent(cls, intent, context: Context):
+    def from_intent(cls, intent, context: Context, name: str = None):
         intent = context.Attributes(intent)
         extent = context.down(intent)
-        return cls(extent, context.up(extent))
+        return cls(extent, context.up(extent), name)
 
     @classmethod
-    def from_extent(cls, extent, context: Context):
+    def from_extent(cls, extent, context: Context, name: str = None):
         extent = context.Objects(extent)
         intent = context.up(extent)
-        return cls(context.down(intent), intent)
+        return cls(context.down(intent), intent, name)
 
     def __repr__(self):
-        return "Concept({}x{})".format(len(self._extent.members()), len(self._intent.members()))
+        if self.name:
+            return "Concept({}, {}x{})".format(self.name, len(self.extent.members()), len(self.intent.members()))
+
+        return "Concept({}x{})".format(len(self.extent.members()), len(self.intent.members()))
 
     def __str__(self):
-        return "Concept({}, {})".format(str(self._extent.members()), str(self._intent.members()))
+        if self.name:
+            return "Concept({}, {}, {})".format(self.name, str(self.extent.members()), str(self.intent.members()))
+
+        return "Concept({}, {})".format(str(self.extent.members()), str(self.intent.members()))
 
     def __eq__(self, other):
         if isinstance(self, type(other)):
-            return (self._extent == other._extent) and (self._intent == other._intent)
+            return (self.extent == other.extent) and (self.intent == other.intent)
 
     def __hash__(self):
-        return (hash(self._extent) ^ hash(self._intent)) ^ hash((self._extent, self._intent))
-
-    @property
-    def extent(self):
-        return self._extent
-
-    @property
-    def intent(self):
-        return self._intent
+        return (hash(self.extent) ^ hash(self.intent)) ^ hash((self.extent, self.intent))
