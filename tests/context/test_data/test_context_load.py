d7d352e7b219bc98efaab61c74698ca2b23fad77
Tomáš Mikula <mail@tomasmikula.cz>
2021-04-14 16:56:48 +0200

    Remove old unittests

--- tests/context/test_data/test_context_load.py
+++ tests/context/test_data/test_context_load.py
@@ -33,60 +33,6 @@ def test_context_from_fimi(data_file, json_file):
         map(tuple, expected_json['bools']))
 
 
-TEST_DATA_DIR_CSV = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'csv', 'no_parameter'
-)
-
-
-@pytest.mark.parametrize("data_file, json_file",
-                         load_all_test_files(TEST_DATA_DIR_CSV))
-def test_context_from_csv(data_file, json_file):
-
-    # Load dataset file
-    with open(data_file) as f:
-        context = Context.from_csv(data_file)
-
-    # Load expected output
-    with open(json_file) as f:
-        expected_json = json.load(f)
-
-    # Compare
-    assert list(context.Attributes.supremum.members(
-    )) == expected_json['attributes']
-    assert list(context.Objects.supremum.members(
-    )) == expected_json['objects']
-    assert tuple(context.to_bools()) == tuple(
-        map(tuple, expected_json['bools']))
-
-
-TEST_DATA_DIR_CSV_DELIMITER = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'csv', 'delimiter'
-)
-
-
-@pytest.mark.parametrize("data_file, json_file",
-                         load_all_test_files(TEST_DATA_DIR_CSV_DELIMITER))
-def test_context_from_csv_delimiter(data_file, json_file):
-
-    # Load dataset file
-    with open(data_file) as f:
-        context = Context.from_csv(data_file, delimiter=';')
-
-    # Load expected output
-    with open(json_file) as f:
-        expected_json = json.load(f)
-
-    # Compare
-    assert list(context.Attributes.supremum.members(
-    )) == expected_json['attributes']
-    assert list(context.Objects.supremum.members(
-    )) == expected_json['objects']
-    assert tuple(context.to_bools()) == tuple(
-        map(tuple, expected_json['bools']))
-
-
 def test_context_from_pandas():
     bools = (
         (2, 0, 6),
