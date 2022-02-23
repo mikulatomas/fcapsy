e0e186b8ba66d08ac82db9d82110b59e32d33eab
Tomáš Mikula <mail@tomasmikula.cz>
2022-02-23 16:57:55 +0100

    v0.3.2 (#17)
    * Add empty_attribute flag to typicality

--- fcapsy/typicality.py
+++ fcapsy/typicality.py
@@ -11,11 +11,13 @@ Proceedings of CLA 2020 (2020): 33-45.
 
 import typing
 import statistics
+import operator
+from functools import reduce
 
 import concepts.lattices
 
 
-__all__ = ["typicality_avg, typicality_min"]
+__all__ = ["typicality_avg", "typicality_min"]
 
 
 def _get_vectors(concept, item):
@@ -32,7 +34,10 @@ def _get_vectors(concept, item):
 
 
 def typicality_min(
-    item: str, concept: "concepts.lattices.Concept", similarity: typing.Callable
+    item: str,
+    concept: "concepts.lattices.Concept",
+    similarity: typing.Callable,
+    empty_attributes: bool = True,
 ) -> float:
     """Calculate typicality of object/attribute in given concept based on worst case (minimal) similarity.
 
@@ -40,34 +45,45 @@ def typicality_min(
         item (str): object or attribute name
         concept (concepts.lattices.Concept)
         similarity (typing.Callable)
+        empty_attributes (bool): if empty attributes (zeros columns) should be included
 
     Example:
         >>> from concepts import Context
         >>> from binsdpy.similarity import jaccard
         >>> context = Context.fromstring('''
-        ...          |2 legs |nests  |flies  |raptor |
-        ... sparrow  |   X   |   X   |   X   |       |
-        ... lark     |   X   |   X   |   X   |       |
-        ... penguin  |   X   |       |       |       |
-        ... chicken  |   X   |   X   |   X   |       |
-        ... vulture  |   X   |       |   X   |   X   |
+        ...          |2 legs |nests  |flies  |raptor |engine |
+        ... sparrow  |   X   |   X   |   X   |       |       |
+        ... lark     |   X   |   X   |   X   |       |       |
+        ... penguin  |   X   |       |       |       |       |
+        ... chicken  |   X   |   X   |   X   |       |       |
+        ... vulture  |   X   |       |   X   |   X   |       |
         ... ''')
         >>> birds = context.lattice.supremum
         >>> typicality_min('sparrow', birds, jaccard) # doctest: +NUMBER
         0.33
         >>> typicality_min('penguin', birds, jaccard) # doctest: +NUMBER
         0.33
+        >>> from binsdpy.similarity import smc
+        >>> typicality_min('penguin', birds, smc, empty_attributes=False) # doctest: +NUMBER
+        0.5
     """
 
     vectors, item_vector = _get_vectors(concept, item)
+    mask = None
 
-    similarities = map(lambda v: similarity(v, item_vector), vectors)
+    if not empty_attributes:
+        mask = reduce(operator.or_, vectors)
+
+    similarities = map(lambda v: similarity(v, item_vector, mask), vectors)
 
     return min(similarities)
 
 
 def typicality_avg(
-    item: str, concept: "concepts.lattices.Concept", similarity: typing.Callable
+    item: str,
+    concept: "concepts.lattices.Concept",
+    similarity: typing.Callable,
+    empty_attributes: bool = True,
 ) -> float:
     """Calculate typicality of object/attribute in given concept based on average similarity.
 
@@ -75,27 +91,35 @@ def typicality_avg(
         item (str): object or attribute name
         concept (concepts.lattices.Concept)
         similarity (typing.Callable)
+        empty_attributes (bool): if empty attributes (zeros columns) should be included
 
     Example:
         >>> from concepts import Context
         >>> from binsdpy.similarity import jaccard
         >>> context = Context.fromstring('''
-        ...          |2 legs |nests  |flies  |raptor |
-        ... sparrow  |   X   |   X   |   X   |       |
-        ... lark     |   X   |   X   |   X   |       |
-        ... penguin  |   X   |       |       |       |
-        ... chicken  |   X   |   X   |   X   |       |
-        ... vulture  |   X   |       |   X   |   X   |
+        ...          |2 legs |nests  |flies  |raptor |engine |
+        ... sparrow  |   X   |   X   |   X   |       |       |
+        ... lark     |   X   |   X   |   X   |       |       |
+        ... penguin  |   X   |       |       |       |       |
+        ... chicken  |   X   |   X   |   X   |       |       |
+        ... vulture  |   X   |       |   X   |   X   |       |
         ... ''')
         >>> birds = context.lattice.supremum
         >>> typicality_avg('sparrow', birds, jaccard) # doctest: +NUMBER
         0.76
         >>> typicality_avg('penguin', birds, jaccard) # doctest: +NUMBER
         0.46
+        >>> from binsdpy.similarity import smc
+        >>> typicality_avg('penguin', birds, smc, empty_attributes=False) # doctest: +NUMBER
+        0.5
     """
 
     vectors, item_vector = _get_vectors(concept, item)
+    mask = None
 
-    similarities = map(lambda v: similarity(v, item_vector), vectors)
+    if not empty_attributes:
+        mask = reduce(operator.or_, vectors)
+
+    similarities = map(lambda v: similarity(v, item_vector, mask), vectors)
 
     return statistics.mean(similarities)
